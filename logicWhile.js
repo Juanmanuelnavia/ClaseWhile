0cd77ebbdfe1e89347384c1019aaa975637132cd
juan manuel <juanmanuelnaviacastillo@gmail.com>
2024-10-25 19:45:41 -0500

    se sube ejercicio while

---
+++ logicWhile.js
@@ -0,0 +1,11 @@
+var numeroEntrada=0;
+var resultado=0;
+var control=0;
+numeroEntrada = parseInt(prompt("ingrese el numero que desea calcular"));
+while (numeroEntrada>=control) {
+    resultado=resultado+control;
+    control=control+1;
+    
+}
+
+alert("la suma de los primeros " +numeroEntrada + " es igual a " + resultado);
